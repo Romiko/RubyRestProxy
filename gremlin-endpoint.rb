c1d667fda873aaac6ad3cf27d38a8f096b82a6dd
Romiko <romikov@gmail.com>
2012-01-26 17:26:34 +1100

    Added RootApiResponse, improve to automate later.

--- gremlin-endpoint.rb
+++ gremlin-endpoint.rb
@@ -16,8 +16,26 @@ post '/raw-gremlin' do
 end
 
 # support some minimalistic exploration for the neo4j-jdbc driver
+  "cypher" : "http://localhost:7474/db/data/cypher",
+  "relationship_index" : "http://localhost:7474/db/data/index/relationship",
+  "node" : "http://localhost:7474/db/data/node",
+  "relationship_types" : "http://localhost:7474/db/data/relationship/types",
+  "neo4j_version" : "1.6.M03",
+  "batch" : "http://localhost:7474/db/data/batch",
+  "extensions_info" : "http://localhost:7474/db/data/ext",
+  "node_index" : "http://localhost:7474/db/data/index/node",
+  "reference_node" : "http://localhost:7474/db/data/node/0",
+  
 get '/' do
-   { :data => request.url ,
-     :extensions => { :GremlinPlugin => { :execute_script => request.url + "raw-gremlin" }}
+   { 
+     :cypher => request.url + "cypher" ,
+	 :relationship_index => request.url + "/db/data/index/relationship" ,
+	 :node => request.url + "/db/data/node" ,
+	 :relationship_types => request.url + "/db/data/relationship/types" ,
+	 :batch => request.url + "/db/data/batch" ,
+	 :extensions_info => request.url + "/db/data/ext" ,
+	 :node_index => request.url + "/db/data/index/node" ,
+     :extensions => { :GremlinPlugin => { :execute_script => request.url + "raw-gremlin" }} ,
+	 :data => request.url
    }.to_json
 end
