2f02b24f950d01192bc30ed2ae8aa6ca4686bbb3
Romiko <romikov@gmail.com>
2012-01-26 19:30:21 +1100

    Add sinatra route for Batch url. Later will use pattern/matching.

--- gremlin-endpoint.rb
+++ gremlin-endpoint.rb
@@ -26,3 +26,14 @@ get '/' do
 	response = RestClient.get ENV['NEO4J_URL'] + '/db/data/', {:content_type => :json, :accept => :json}
 	response.gsub("http://c05f56903.hosted.neo4j.org:7006", "http://neo4jorbust.heroku.com")
 end
+
+post '/db/data/batch' do
+    data = request.body.read;
+    begin
+      data = JSON.parse(data)
+    rescue
+    end 
+    data = {:query => data } unless data.kind_of?(Hash)
+    rest["/db/data/batch"].post data.to_json, 
+                 {:accept=>"application/json",:content_type=>"application/json"}
+end
