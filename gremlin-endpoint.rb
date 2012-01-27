cba2f81cf07a9cca6d38a1048489f6324b7f4a0f
Romiko <romikov@gmail.com>
2012-01-27 18:06:56 +1100

    Add delete handler

--- gremlin-endpoint.rb
+++ gremlin-endpoint.rb
@@ -43,6 +43,6 @@ post '/db/data/batch' do
 end
 
 delete '/db/data/node/:nodeid' do
-	response = RestClient.delete address ENV['NEO4J_URL'] + '/db/data/node/' + params[:nodeid]
+	response = rest.delete[ENV['NEO4J_URL'] + '/db/data/node/' + params[:nodeid]]
 	response.gsub(/(http:\/\/\w+\W*.*\/db\/data)/, "http://" + ENV['APP_NAME']  + ".heroku.com/db/data")
 end
